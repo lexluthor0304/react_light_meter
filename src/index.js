79fed1be9de324fec5a2c6b1e5971d26bc0eded1
lex <lexluthor0304@gmail.com>
2025-03-06 13:44:12 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWrapper from './App';
+import AppWrapper from './App.js';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 
